eb5cf69514490f693fdc8b0347a597747165b22e
“Woo TaeHyeon <wth2052@gmail.com>
2023-05-31 00:02:56 +0900

    ADD: Day4,5 번역완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './todo/todo.controller';
+import { TodoController } from './features/todo/todo.controller';
+import { TodoModule } from './features/todo/todo.module';
+import { TodoService } from './features/todo/todo.service';
+import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
+import { CopyTodoController } from './features/copy-todo/copy-todo.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TodoController],
-  providers: [AppService],
+  imports: [TodoModule, CopyTodoModule],
+  controllers: [AppController, TodoController, CopyTodoController],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
