4a224ad1cc6490b63410628cad840a7a81ca0047
“Woo TaeHyeon <wth2052@gmail.com>
2023-05-31 21:19:24 +0900

    ADD: DAY07 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { TodoModule } from './features/todo/todo.module';
 import { TodoService } from './features/todo/todo.service';
 import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
 import { CopyTodoController } from './features/copy-todo/copy-todo.controller';
+import { HandsomeModule } from './handsome/handsome.module';
 
 @Module({
-  imports: [TodoModule, CopyTodoModule],
+  imports: [TodoModule, CopyTodoModule, HandsomeModule],
   controllers: [AppController, TodoController, CopyTodoController],
   providers: [AppService, TodoService],
 })
