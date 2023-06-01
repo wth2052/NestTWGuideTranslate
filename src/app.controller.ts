86875e60e108919efa082cf3eb7f9787e4f0ece3
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-01 23:27:03 +0900

    ADD: DAY8 번역 완료

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CustomException } from './exceptions/custom.exception';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
+    throw new CustomException();
     return this.appService.getHello();
   }
 }
