c6e927f72d5a6cf5f91057bd857423318f54ade5
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-09 14:51:29 +0900

    ADD:Day15 번역 끝

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { Auth } from './decorators/auth/auth.decorator';
-import { Roles } from './decorators/roles/roles.decorator';
-import { User } from './decorators/user/user.decorator';
-import { RoleGuard } from './guards/role/role.guard';
+import { Controller, Get } from '@nestjs/common';
+import { ConfigurationService } from './common/configuration/configuration.service';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(
+    private readonly configService: ConfigurationService
+  ) {
+  }
 
-  @Auth('staff')
   @Get()
-  getHello(@User('name') name: string): string {
-    return name;
+  getHello() {
+    return { username: this.configService.get('USERNAME') };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AddUserMiddleware } from './middlewares/add-user/add-user.middleware';
+import { ConfigurationModule } from './common/configuration/configuration.module';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigurationModule.forRoot({
+      path: `../${process.env.NODE_ENV || 'development'}.env`
+    })
+  ],
+  controllers: [
+    AppController
+  ],
   providers: [
     AppService
   ]
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AddUserMiddleware).forRoutes('');
-  }
+export class AppModule {
 }
