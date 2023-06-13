3359e716e6fa7075e43f53b9417171d185ce14dc
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-13 21:55:45 +0900

    ADD: Day18번역 끝

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import { Module, Scope } from '@nestjs/common';
+import { Module, OnApplicationShutdown } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StorageModule } from './common/storage/storage.module';
-import { BookModule } from './common/book/book.module';
 
 @Module({
-  imports: [StorageModule, BookModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'USERNAME',
-      useValue: 'WOO',
-      scope: Scope.REQUEST, // scope 속성 추가
-    },
+  imports: [
   ],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService
+  ]
 })
-export class AppModule {}
+export class AppModule implements OnApplicationShutdown {
+  onApplicationShutdown(): void {
+    console.log('[AppModule]: shutdown event!');
+  }
+}
