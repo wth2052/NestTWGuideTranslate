764771390ee2bb114d165d66d9ec6237f0d6822a
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-16 17:13:27 +0900

    ADD: 단일 form 다중 파일 업로드

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,19 @@
 import {
   Controller,
   Post,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
-  @Post('/single')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
-    return file;
+  @Post('/multiple')
+  @UseInterceptors(FileInterceptor('files'))
+  uploadSingleFile(@UploadedFiles() files: Express.Multer.File[]) {
+    return files.map(({ fieldname, originalname }) => ({
+      fieldname,
+      originalname,
+    }));
   }
 }
