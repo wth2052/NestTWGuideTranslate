916b1aec26ae7f41b878d0361b1fc0bef64f57c4
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-16 17:12:22 +0900

    ADD: Day20 이미지 업로드 번역 진행중, 단일form 단일 파일 업로드

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,16 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { ContextIdFactory, ModuleRef, REQUEST } from '@nestjs/core';
-
-import { Request } from 'express';
-
-import { AppService } from './app.service';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly moduleRef: ModuleRef,
-    @Inject(REQUEST) private readonly request: Request
-  ) {}
-
-  @Get()
-  async getTruth() {
-    const identifier = ContextIdFactory.getByRequest(this.request);
-    const [instance1, instance2] = await Promise.all([
-      this.moduleRef.resolve(AppService, identifier),
-      this.moduleRef.resolve(AppService, identifier)
-    ]);
-
-    return instance1 === instance2;
+  @Post('/single')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadSingleFile(@UploadedFile() file: Express.Multer.File) {
+    return file;
   }
-
 }
