8b7ac7a9eaf977af58afe6974b15e7ced1963543
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-19 22:34:54 +0900

    ADD: Day22 진행중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-const MONGO = {
-  username: '<Example>',
-  password: encodeURIComponent('<YOUR_PASSWORD>'),
-  getUrl: function () {
-    return `mongodb+srv://${this.username}:${this.password}@<YOUR_DB>`
-  }
-};
+import MongoConfigFactory from './config/mongo.config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(MONGO.getUrl())
+    ConfigModule.forRoot({
+      load: [MongoConfigFactory]
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('mongo.uri')
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/common/models/todo.model.ts
+++ src/common/models/todo.model.ts
@@ -1,6 +1,22 @@
-export interface Todo {
-	userId: number;
-	id: number;
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { User } from './user.model';
+
+export type TodoDocument = Todo & Document;
+
+@Schema()
+export class Todo {
+
+	@Prop({ required: true, maxlength: 20 })
 	title: string;
+
+	@Prop({ maxlength: 200 })
+	description: string;
+
+	@Prop({ required: true })
 	completed: boolean;
+
+	@Prop({ type: Types.ObjectId, ref: 'User' })
+	owner: User;
+
 }
