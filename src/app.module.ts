bef974193c9dacf60af8d7951183c71d6b6ce33b
“Woo TaeHyeon <wth2052@gmail.com>
2023-06-29 00:46:56 +0900

    FIX: dirname issue -> src 경로

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,9 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     AuthorizationModule.register({
-      modelPath: join(__dirname, '../casbin/model.conf'),
-      policyAdapter: join(__dirname, '../casbin/policy.csv'),
+      //윈도우 dirname 이슈, src 경로로 변경
+      modelPath: join('src/casbin/model.conf'),
+      policyAdapter: join('src/casbin/policy.csv'),
       global: true,
     }),
     TodoModule,
