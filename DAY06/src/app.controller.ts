0dd411fc9e5603b1adbb151ba8943ad712b89f51
wth2052 <wth2052@gmail.com>
2023-10-21 19:58:51 +0900

    ADD: DAY06 폴더

--- DAY06/src/app.controller.ts
+++ DAY06/src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('ALIAS_APP_SERVICE') private readonly alias: AppService
+  ) {
+    console.log(this.alias === this.appService); // 비교 진행
+  }
 
   @Get()
   getHello(): string {
--- DAY06/src/app.module.ts
+++ DAY06/src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './features/todo/todo.controller';
-import { TodoModule } from './features/todo/todo.module';
-import { TodoService } from './features/todo/todo.service';
-import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
-import { CopyTodoController } from './features/copy-todo/copy-todo.controller';
-import { HandsomeModule } from './handsome/handsome.module';
 
 @Module({
-  imports: [TodoModule, CopyTodoModule, HandsomeModule],
-  controllers: [AppController, TodoController, CopyTodoController],
-  providers: [AppService, TodoService],
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'ALIAS_APP_SERVICE',
+      useExisting: AppService
+    }
+  ],
 })
 export class AppModule {}
